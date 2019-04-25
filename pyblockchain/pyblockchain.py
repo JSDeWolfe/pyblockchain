1d441c89946d5afa78e275e15d11a10fd3b2b78b
James <james@trueface.ai>
2019-04-25 14:38:18 -0700

    m

--- pyblockchain/pyblockchain.py
+++ pyblockchain/pyblockchain.py
@@ -306,6 +306,23 @@ def chaintest():
 @app.route('/posttransaction', methods=['POST'])
 def posttransaction():
     values = request.get_json()
+
+    # Check that the required fields are in the POST'ed data
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    # Create a new Transaction
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return jsonify(response), 201
+
+
+
+@app.route('/posttransaction', methods=['GET'])
+def posttransaction():
+    values = request.get_json()
     return jsonify(values), 200
 
 
